7b86560d5d664792478be01da31f3ea0a5a5c626
Wu-ShengY <syseanwu@umich.edu>
2025-03-18 20:59:17 -0400

    Update CRO_main.jl

--- CRO/CRO_main.jl
+++ CRO/CRO_main.jl
@@ -19,7 +19,7 @@ function parse_commandline()
         "--samples", "-s"
             help = "Number of Samples to run distribution"
             arg_type = Int64
-            default = 20
+            default = 50
         "--alpha", "-a"
             help = "Dataset Adjacency"
             arg_type = Float64
@@ -31,15 +31,15 @@ function parse_commandline()
         "--magitude", "-m"
             help = "Magitudes of attack"
             arg_type = Float64
-            default = 0.1
+            default = 0.05
         "--Psi", "-p"
             help = "Parameters for constraint violation in OPF"
             arg_type = Float64
-            default = 1000
+            default = 200
         "--beta", "-b"
             help = "Trade off parameters β in CRO algorithm"
             arg_type = Float64
-            default = 1
+            default = 5
         "--gamma", "-g"
             help = "regularization parameters γ in CRO algorithm"
             arg_type = Float64
@@ -52,10 +52,15 @@ args = parse_commandline()
 att = Dict(:δ̅ => net[:d]*args["magitude"], :δ̲ => -net[:d]*args["magitude"], :Ψ =>args["Psi"], :τ=>args["tau"])
 syn_set=Dict(:α =>args["alpha"] , :c_max =>0.0, :ϵ =>args["loss"], :γ=>args["gamma"], :β=>args["magitude"],  :s=>args["samples"])
 
-# To get numerical results
-dict_pp, dict_cro=Results_CRO(100)
-@show [mean(dict_cro[:C̃]) std(dict_cro[:C̃]) mean(dict_cro[:C_OPF]) std(dict_cro[:C_OPF]) mean(dict_cro[:C_att_BO]) std(dict_cro[:C_att_BO]) mean(dict_cro[:C_att_RO]) std(dict_cro[:C_att_RO])]
+# To get numerical results of normal and post-attack OPF cost in PJM 5-bus systems
+# dict_pp, dict_cro=Results_CRO(100)
+# @show [mean(dict_cro[:C̃]) std(dict_cro[:C̃]) mean(dict_cro[:C_OPF]) std(dict_cro[:C_OPF]) mean(dict_cro[:C_att_BO]) std(dict_cro[:C_att_BO]) mean(dict_cro[:C_att_RO]) std(dict_cro[:C_att_RO])]
 
-# To get figures
+# Get results and serialize them
+# CaseStudy_CRO(syn_set[:s])
 
+# To get figures from saved results
 # plot_CRO()
+
+# To generate new figures from scratch
+plot_CRO_test(syn_set[:s])
